fbb2b822c8619872c78e2f800e9ef4d1637dc89d
Shahbaz Javeed <sjaveed@gmail.com>
2015-04-22 17:43:58 +0400

    Initial Commit

---
+++ ClickCounterWithStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickCounterWithStoryboard
+//
+//  Created by Shahbaz Javeed on 4/22/15.
+//  Copyright (c) 2015 Shahbaz Javeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
