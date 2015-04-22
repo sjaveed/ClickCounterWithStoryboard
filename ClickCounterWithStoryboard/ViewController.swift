37fdb0e231807cfdeb7196c537e4641b9fa350d3
Shahbaz Javeed <sjaveed@gmail.com>
2015-04-22 17:58:47 +0400

    * Replicated basic ClickCounter functionality with Storyboards

--- ClickCounterWithStoryboard/ViewController.swift
+++ ClickCounterWithStoryboard/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func incrementCount() {
+        count++
+        label.text = "\(count)"
+    }
 
 }
 
